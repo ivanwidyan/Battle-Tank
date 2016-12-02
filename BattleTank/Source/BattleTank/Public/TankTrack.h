e957f3bd6cf13d3d510034f01a2171b876632297
Ivan Widyan <ivanwidyan@yahoo.com>
2016-12-02 23:09:53 +0100

    Quit Button

---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
