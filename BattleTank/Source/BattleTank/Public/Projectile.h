393113a99948f5137d97e63ba37bddcaecf92ce0
Ivan Widyan <ivanwidyan@yahoo.com>
2017-01-14 20:28:32 +0100

    Particle Systems

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -28,5 +28,6 @@ private:
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UStaticMeshComponent* CollisionMesh = nullptr;
 
+	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UParticleSystemComponent* LaunchBlast = nullptr;
 };
